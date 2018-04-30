25165dc3cc41c6892ab976e39dd3401ec86236d4
MariaTsantaki <Maria.Tsantaki@astro.up.pt>
2018-04-30 17:58:34 +0100

    interpolate on the wave of the synthetic

--- utils.py
+++ utils.py
@@ -81,69 +81,25 @@ def prepare_linelist(linelist, wavelengths):
     return s.reshape(1, -1)
 
 # These functions and the observations.py should go to the spectrum.py
-def read_linelist(fname='moog_apogee_atom_cn_clean.lst', intname='intervals.lst'):
-    '''Read the line list return atomic data and ranges
-
-    Input
-    -----
-    fname : str
-      File that contains the linelist
-    intname : str
-      File that contains the intervals
-
-    Output
-    ------
-    ranges : wavelength ranges of the linelist
-    atomic : atomic data
-    '''
-
-    if not os.path.isfile(intname):
-        raise IOError('The interval list is not in the correct place!')
-    if not os.path.isfile(fname):
-        raise IOError('The line list is not in the correct place!')
-    lines = pd.read_csv(fname, skiprows=1, comment='#', delimiter=r'\s+', usecols=range(6),
-    names=['wl', 'elem', 'excit', 'loggf', 'vdwaals', 'Do'],
-    converters={'Do': lambda x : x.replace("nan"," "), 'vdwaals': lambda x : float(x)})
-    lines.sort_values(by='wl', inplace=True)
-
-    intervals = pd.read_csv(intname, comment='#', names=['start', 'end'], delimiter='\t')
-    ranges = intervals.values
-    atomic = []
-    for i, ri in enumerate(intervals.values):
-        a = lines[(lines.wl>ri[0]) & (lines.wl<ri[1])]
-        a = a.as_matrix()
-        atomic.append(a)
-    atomic = np.vstack(atomic)
-    N = len(atomic)
-    print('Linelist contains %s lines in %s intervals' % (N, len(ranges)))
-
-    # Create line list for MOOG
-    fmt = ["%9s", "%8s", "%9s", "%9s", "%6s", "%8s"]
-    header = 'Wavelength     ele       EP      loggf   vdwaals   Do'
-    np.savetxt('linelist.moog', atomic, fmt=fmt, header=header)
-    return ranges, atomic
-
 def prepare_spectrum(spectrum):
     from scipy.interpolate import InterpolatedUnivariateSpline
 
-    r, a = read_linelist('moog_apogee_atom_cn_clean_short.lst', intname='intervals.lst')
-    #wl = np.array([])
-    #for i in np.arange(8575): wl = np.append(wl, 4.179 + 6.E-6*i)
-    #wl = 10.**(wl)
+    if not os.path.isfile(intname):
+        raise IOError('The interval list is not in the correct place!')
 
-    #w = []
-    #for i, ri in enumerate(r):
-    #    wll = wl[np.where((wl >= ri[0]) & (wl <= ri[1]))]
-    #    w.append(wll)
+    intervals = pd.read_csv(intname, comment='#', names=['start', 'end'], delimiter='\t')
+    ranges = intervals.values
 
-    #w = np.array(w)
-    # This is something weird :)
-    hdulist = fits.open('/home/paranoia/mex/APOGEE_dwarfs_synthetic/results/6800_4.381_0.28_1.75_7.81_2.0_0.0_22500.spec')
-    x = hdulist[1].data
-    w = x['wavelength']
-    f = x['flux']
+    # This is the wavelength of the synthetic spectra
+    wlin = []
+    for i, ri in enumerate(ranges):
+        N = abs((ri[0] - ri[1])/0.1)
+        x = np.linspace(ri[0]+0.1, ri[1], num=int(N-1), endpoint=False)
+        wlin.append(x)
+    wlin = np.array(wlin)
+    w = np.hstack(wlin)
 
-    wave, flux, d = read_obs_intervals(spectrum, r, snr=100)
+    wave, flux, d = read_obs_intervals(spectrum, ranges, snr=100)
     sl = InterpolatedUnivariateSpline(wave, flux, k=1)
     int_flux = sl(w)
     return int_flux.reshape(1, -1)
