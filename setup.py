1cde6f659da9a7799bc1e7aebe7ab40b55b0463e
DanielAndreasen <daniel.andreasen@hotmail.com>
2018-08-15 17:12:17 +0200

    Increase to version 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from Cython.Distutils import build_ext
 setup(
 	maintainer='Maria Tsantaki and Daniel Andreasen',
     name='specML',
-	version=0.1,
+	version=0.2,
     packages=find_packages(),
     url='https://github.com/MariaTsantaki/spectroscopy-ML',
     cmdclass={'build_ext': build_ext},
